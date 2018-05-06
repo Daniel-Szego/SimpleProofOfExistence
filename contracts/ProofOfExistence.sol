4d814a8e27387f2d90335533d029ceec2baed7e3
Daniel <szego.daniel@gmail.com>
2018-05-06 03:40:40 -0700

    refactor and unit test

--- contracts/ProofOfExistence.sol
+++ contracts/ProofOfExistence.sol
@@ -2,7 +2,7 @@
 contract ProofOfExistence {
 
     address public owner;
-    mapping(address => bytes32) public hashes;     
+    mapping(address => string) public hashes;     
 
     function ProofOfExistance(){
         owner = msg.sender;
@@ -13,30 +13,19 @@ contract ProofOfExistence {
         _;
     }
 
-    function RegisterHash(string input){
-        hashes[msg.sender] = keccak256(input);
+    function RegisterHash(string _input){
+        hashes[msg.sender] = _input;
     }
 
     function GetHashSting(address _address) view returns (string){
-        return bytes32ToString(hashes[_address]);
+        return hashes[_address];
     }
 
-    function bytes32ToString(bytes32 x) constant returns (string) {
-        bytes memory bytesString = new bytes(32);
-        uint charCount = 0;
-        for (uint j = 0; j < 32; j++) {
-            byte char = byte(bytes32(uint(x) * 2 ** (8 * j)));
-            if (char != 0) {
-                bytesString[charCount] = char;
-                charCount++;
-            }
-        }
-        bytes memory bytesStringTrimmed = new bytes(charCount);
-        for (j = 0; j < charCount; j++) {
-            bytesStringTrimmed[j] = bytesString[j];
-        }
-        return string(bytesStringTrimmed);
+    function GetHashFromString(string _input) view returns (bytes32) {
+        bytes32 hash = keccak256(_input);
+        return hash;
     }
+
 
     function kill() isAdmin{
         selfdestruct(owner);
---
+++ test/01_TestWrite.js
@@ -0,0 +1,21 @@
+var ProofOfExistence = artifacts.require("ProofOfExistence");
+var ProofOfExistenceInstance;
+var hashStringInput;
+var hashStringOutout;
+
+contract('ProofOfExistence', function(accounts) {
+    it("test ProofOfExistence register hash", function() {
+        return ProofOfExistence.deployed().then(function(instance) {
+            ProofOfExistenceInstance = instance;
+            return ProofOfExistenceInstance.GetHashFromString("Hello World", {from: accounts[0]});             
+        }).then(function(result) {
+            hashStringInput = result;
+            return ProofOfExistenceInstance.RegisterHash(hashStringInput, {from: accounts[0]});             
+        }).then(function(result) {
+            return ProofOfExistenceInstance.GetHashSting(accounts[0], {from: accounts[0]});             
+        }).then(function(result) {
+            hashStringOutout = result;
+            assert.equal(hashStringInput, hashStringOutout, "Hashes are equal");            
+        });
+    });
+});
